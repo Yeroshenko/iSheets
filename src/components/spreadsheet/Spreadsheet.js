1fc5f88569cd7080cac3e9b20c11dd160f1eed6e
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-06-22 23:32:48 +0300

    feat: added Dom methods

--- src/components/spreadsheet/Spreadsheet.js
+++ src/components/spreadsheet/Spreadsheet.js
@@ -2,7 +2,7 @@ import { $ } from '@core'
 
 export class Spreadsheet {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
@@ -13,7 +13,7 @@ export class Spreadsheet {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
 
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
 
--- src/core/Dom.js
+++ src/core/Dom.js
@@ -1,15 +1,50 @@
 class Dom {
-  constructor() {}
+  constructor(selector) {
+    this.$el =
+      typeof selector === 'string' ? document.querySelector(selector) : selector
+  }
+
+  // element.innerHTML 2.0
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  on() {}
+
+  // ParentNode.appendChild() 2.0
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+
+    Element.prototype.append
+      ? this.$el.append(node)
+      : this.$el.appendChild(node)
+
+    return this
+  }
 }
 
-export const $ = () => {
-  return new Dom()
+// event.target
+export const $ = selector => {
+  console.log(new Dom(selector))
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
   const el = document.createElement(tagName)
 
-  if (classes) el.classList.add(classes)
-
-  return el
+  if (classes) {
+    el.classList.add(classes)
+  }
+  return $(el)
 }
