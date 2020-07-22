62b0d9c12ee4e002d92075a0b1000ec8566fc478
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-07-22 16:38:29 +0300

    test: create createStore.test

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleNameMapper: {
+    // '^@(.*)$': '<rootDir>/src$1',
+    '^@core(.*)$': '<rootDir>/src/core$1',
+    '^@store(.*)$': '<rootDir>/src/store$1',
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@components(.*)$': '<rootDir>/src/components$1'
+  }
+}
