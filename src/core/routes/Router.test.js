7d1d462a44255deb743a5d7f3b25376c57dbdf81
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-07-23 13:42:01 +0300

    test: create createStore.test

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,34 @@
+import { Router } from './Router'
+import { Page } from '../Page'
+
+class DashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'dashboard'
+    return root
+  }
+}
+class SpreadsheetPage extends Page {}
+
+describe('Router', () => {
+  let router
+  let $root
+
+  beforeEach(() => {
+    $root = document.createElement('div')
+
+    router = new Router($root, {
+      dashboard: DashboardPage,
+      spreadsheet: SpreadsheetPage
+    })
+  })
+
+  test('should be defined', () => {
+    expect(router).toBeDefined()
+  })
+
+  test('should render Dashboard Page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>dashboard</div>')
+  })
+})
